31da6ef35e701f8c01cd48d4085d0c90fa30239b
FiR3bL4z3 <horvathb0222@gmail.com>
2023-04-03 17:45:18 +0200

    Release 0.1.3

--- client/src/index.ts
+++ client/src/index.ts
@@ -9,10 +9,12 @@ export {
   useWPSendForm,
   useWindowDimensions,
 } from "./hooks";
-
+export { useBringContext, BringContextProvider } from "./context/bring-context";
 export { clientInit } from "./client-init";
 export {
   BringStylesDefaultValue,
   makeBringStylesClassNames,
   makeResponsiveClassNames,
 } from "./styles";
+
+export { postContent } from "./components/post-content";
