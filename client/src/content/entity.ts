f025dcf6a9bec60a5fb637379e926dc3bc298520
Balázs Horváth <44926163+FiR3bL4z3@users.noreply.github.com>
2024-03-19 10:32:42 +0100

    Extend getEntity return type (#2)

--- client/src/content/entity.ts
+++ client/src/content/entity.ts
@@ -1,5 +1,20 @@
 import type {Entity} from "../types";
 
+type NotFoundResponse = {
+	responseCode: 404;
+	slug: "not_found";
+	type: "general";
+};
+type RedirectResponse = {
+	responseCode: 301 | 302 | 307;
+	redirectTo: string;
+};
+
+export type GetEntityReturnType<EP> =
+	| Entity<EP>
+	| NotFoundResponse
+	| RedirectResponse;
+
 async function getEntity<EP = {}>(url: string, slug: string | string[] = "") {
 	const joinedSlug = typeof slug === "string" ? slug : slug.join("/");
 
@@ -11,7 +26,7 @@ async function getEntity<EP = {}>(url: string, slug: string | string[] = "") {
 			},
 		});
 
-		const responseData = (await response.json()) as Entity<EP>;
+		const responseData = (await response.json()) as GetEntityReturnType<EP>; // TODO: parse with zod
 		return responseData;
 	} catch (error) {
 		console.error(error);
