11121544463bb886b5288533434313e14ce31d45
Balázs Horváth <44926163+FiR3bL4z3@users.noreply.github.com>
2024-07-25 13:42:46 +0200

    Refactor editor package to new export syntax (#15)

--- packages/blocks-editor/tsup.config.ts
+++ packages/blocks-editor/tsup.config.ts
@@ -1,7 +1,17 @@
 import {defineConfig} from "tsup";
 
 export default defineConfig({
-	entry: ["src/index.ts"],
+	entry: [
+		"src/index.ts",
+		"src/blocks/index.ts",
+		"src/components/index.tsx",
+		"src/controls/index.ts",
+		"src/hooks/index.ts",
+		"src/styles/index.ts",
+		"src/editor.ts",
+		"src/types.ts",
+		"src/utils.ts",
+	],
 	format: ["cjs", "esm"],
 	external: [
 		"@wordpress/blocks",
