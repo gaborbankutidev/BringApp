6cf01fe31f2fcabe9df6b4faf377edd83f5d5243
blaze <horvathb0222@gmail.com>
2024-07-31 23:12:50 +0200

    Fix app after-install script

--- apps/bring-app/scripts/after-install.mjs
+++ apps/bring-app/scripts/after-install.mjs
@@ -3,6 +3,4 @@ import {execSync} from "child_process";
 if (process.env.SKIP_COMPOSER_INSTALL) {
 	process.exit(0);
 }
-
-execSync("yarn install:composer:pkg", {stdio: "inherit"});
-execSync("yarn install:composer:app", {stdio: "inherit"});
+execSync("composer install", {stdio: "inherit"});
