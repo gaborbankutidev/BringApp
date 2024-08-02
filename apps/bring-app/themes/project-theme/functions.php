71b62ab7d499ee95cf2ac4d6d0eb5a60055a6ce7
Tamas Fazakas <tom.fazakas@gmail.com>
2024-08-02 10:11:18 +0200

    Define project plugins in project child theme

--- apps/bring-app/themes/project-theme/functions.php
+++ apps/bring-app/themes/project-theme/functions.php
@@ -1,6 +1,14 @@
-<?php 
-	 add_action( 'wp_enqueue_scripts', 'project_theme_enqueue_styles' );
-	 function project_theme_enqueue_styles() {
- 		  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
- 		  } 
- ?>
+<?php
+
+declare(strict_types=1);
+
+// No direct access
+defined("ABSPATH") or die("Hey, do not do this 😱");
+
+add_action("wp_enqueue_scripts", "project_theme_enqueue_styles");
+function project_theme_enqueue_styles() {
+	wp_enqueue_style("parent-style", get_template_directory_uri() . "/style.css");
+}
+
+require_once get_stylesheet_directory() . "/plugins.php";
+?>
