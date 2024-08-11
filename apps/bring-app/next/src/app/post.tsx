5ed5702bd473fcb6f9ef7e58ea52f82c973dc0b7
Armin Benard <arminbenard@gmail.com>
2024-08-12 01:39:07 +0200

    cleanup post component

--- apps/bring-app/next/src/app/post.tsx
+++ apps/bring-app/next/src/app/post.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import {useDynamicEntityList} from "@bring/blocks-client";
+import {twJoin} from "tailwind-merge";
 
 export default function Post() {
 	const {ref, entityList} = useDynamicEntityList(
@@ -13,17 +14,25 @@ export default function Post() {
 
 	return (
 		<div ref={ref}>
-			{entity && (
-				<div className="bg-gray-800/60 border border-gray-500/60 px-4 py-8 rounded-lg md:w-1/2 hover:bg-gray-700/60 cursor-pointer">
-					<p className="text-14 text-red-500 mb-2">
-						Example post from WordPress
-					</p>
-					<h3 className="text-24s mb-4">{entity.name}</h3>
+			<div
+				className={twJoin(
+					"bg-gray-800/60 min-h-[180px] min-w-[280px] border border-gray-500/60 px-4 py-8 rounded-lg md:w-1/2 hover:bg-gray-700/60 cursor-pointer",
+					!entity && "animate-pulse",
+				)}
+			>
+				{entity && (
+					<>
+						<p className="text-14 text-red-500 mb-2">
+							Example post from WordPress
+						</p>
+						<h3 className="text-24s mb-4">{entity.name}</h3>
 
-					{/* TODO fetch excerpt too */}
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-				</div>
-			)}
+						{/* TODO fetch excerpt too */}
+						{/* <p>{entity.excerpt}</p> */}
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+					</>
+				)}
+			</div>
 		</div>
 	);
 }
