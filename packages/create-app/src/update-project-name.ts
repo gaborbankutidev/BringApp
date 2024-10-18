8be49a7e0ac9d14fbf5dcad5b789cfb3b56ae48a
Tamas Fazakas <tom.fazakas@gmail.com>
2024-10-18 22:53:07 +0200

    Add project name to plugin name

--- packages/create-app/src/update-project-name.ts
+++ packages/create-app/src/update-project-name.ts
@@ -13,4 +13,30 @@ export function updateProjectName(directory: string, projectName: string) {
 
 	fsExtra.writeJsonSync(packageJsonPath, packageJson, {spaces: 2});
 	fsExtra.writeJsonSync(composerJsonPath, composerJson, {spaces: 2});
+
+	// Update wordpress plugin name
+	updatePluginName(directory, projectName);
+}
+
+function updatePluginName(directory: string, projectName: string) {
+	// Define the initial and updated plugin names
+	const initialPluginName = "Plugin Name:       Bring App";
+	const updatedPluginName = `Plugin Name:       Bring App | ${projectName}`;
+
+	// Construct the plugin file path
+	const pluginPath = path.join(directory, "plugins/bring-app/bring-app.php");
+
+	try {
+		// Read the plugin file content
+		const pluginFileContent = fsExtra.readFileSync(pluginPath, {encoding: "utf-8"});
+
+		// Replace the initial plugin name with the updated one
+		const updatedContent = pluginFileContent.replace(initialPluginName, updatedPluginName);
+
+		// Write the updated content back to the file
+		fsExtra.writeFileSync(pluginPath, updatedContent);
+	} catch (error) {
+		// Handle any errors during file read/write operations
+		console.error(`Failed to update plugin name in ${pluginPath}:`, error);
+	}
 }
