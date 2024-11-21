8d5707bb5989b6053d9dc625ea490c7ec8f71c7a
tom3rr <68561287+tom3rr@users.noreply.github.com>
2024-11-21 16:52:02 +0100

    Refactor & enhance useWPSendForm hook (#96)
    * Refactor useWPSendForm hook for proper mutation state and response handling
    
    * Update changeset
    
    * Fix incoherent send form options type usage

---
+++ .changeset/forty-glasses-agree.md
@@ -0,0 +1,5 @@
+---
+"@bring/blocks-client": minor
+---
+
+Refactor and enhance useWPSendForm hook
--- packages/blocks-client/src/hooks/use-wp-send-form.ts
+++ packages/blocks-client/src/hooks/use-wp-send-form.ts
@@ -8,32 +8,32 @@ import {UseSendFormOptions, useSendForm} from "./use-send-form";
  * @param wpURL - The URL of the WordPress site.
  * @returns A function that can be used to send form data and the state of the form submission.
  *
- * @template PayloadT - The type of the form data payload.
+ * @template SuccessT - The type of the success response.
+ * @template ErrorT - The type of the error response.
+ * @template FormDataPayloadT - The type of the form data payload.
  */
-export const makeUseWPSendForm = (wpURL?: string | undefined) => {
+export const makeUseWPSendForm = (wpURL?: string) => {
 	const defaultFormUrl = `${wpURL ?? ""}/wp-json/bring/form/submit`;
 
-	return <PayloadT>(
+	return <SuccessT, ErrorT, FormDataPayloadT>(
 		formName: string,
-		options?: UseSendFormOptions<
-			{success: true; message: string},
-			{success: false; message: string},
-			{formName: string; formData: PayloadT}
-		> & {
-			formUrl?: string;
-		},
+		options?: {formUrl?: string} & UseSendFormOptions<
+			SuccessT,
+			ErrorT,
+			{formName: string; formData: FormDataPayloadT}
+		>,
 	) => {
-		const useSendFormResult = useSendForm<
-			{success: true; message: string},
-			{success: false; message: string},
-			{formName: string; formData: PayloadT}
-		>(options?.formUrl ?? defaultFormUrl, options);
-		const send = async (formData: PayloadT) => {
-			useSendFormResult.send({formName, formData});
-		};
+		const {formUrl = defaultFormUrl, ...sendFormOptions} = options || {};
+
+		const {sendAsync, ...rest} = useSendForm<
+			SuccessT,
+			ErrorT,
+			{formName: string; formData: FormDataPayloadT}
+		>(formUrl, sendFormOptions);
+
 		return {
-			send,
-			state: useSendFormResult.state,
+			send: (formData: FormDataPayloadT) => sendAsync({formName, formData}),
+			...rest,
 		};
 	};
 };
