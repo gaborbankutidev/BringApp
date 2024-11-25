1fd5036a1fe067f04fa158ef09fb7341f14728f4
Gábor Bánkuti <gaborbankuti93@gmail.com>
2024-11-25 09:02:51 +0100

    Add SearchWP to Extend

---
+++ apps/bring-app/plugins/bring-app/src/Extend/SearchWP/Stubs.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SearchWP;
+
+class Query {
+	/**
+	 * @param string $term
+	 * @param array<string,string> $args
+	 *
+	 * @phpstan-ignore-next-line
+	 */
+	public function __construct($term, $args = []) {
+	}
+
+	/**
+	 * @return array<object{"id": string, "source": string, "relevance": string}>
+	 *
+	 * @phpstan-ignore-next-line
+	 */
+	public function get_results() {
+	}
+}
