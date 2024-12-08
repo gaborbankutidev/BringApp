c15c4a48af9b86f0c745ba6dd69fee1c1d5c880b
IJustMatthew <sera.mate92@gmail.com>
2024-12-08 11:01:05 +0100

    add login-form storybook

---
+++ apps/bring-app/next/src/components/ui/form/login/login-form.stories.tsx
@@ -0,0 +1,11 @@
+import type {Meta} from "@storybook/react";
+
+import LoginForm from "./login-form";
+
+const meta = {
+	title: "Components/Ui/LoginForm",
+	component: LoginForm,
+	tags: ["autodocs"],
+} satisfies Meta<typeof LoginForm>;
+
+export default meta;
