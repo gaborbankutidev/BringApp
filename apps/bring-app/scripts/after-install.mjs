df183de6f48e565db702809fefaef140311675a0
Tamas Fazakas <tom.fazakas@gmail.com>
2024-12-09 16:41:06 +0100

    Refactor after install scripts

--- apps/bring-app/scripts/after-install.mjs
+++ apps/bring-app/scripts/after-install.mjs
@@ -2,23 +2,27 @@ import { execSync } from "child_process"
 import fs from "fs"
 import path from "path"
 
-if (!process.env.SKIP_GENERATE_COMPOSER_AUTH) {
-	const composerAuth = process.env.COMPOSER_AUTH
+export function generateComposerAuthJson(filePath = "") {
+	if (!process.env.SKIP_GENERATE_COMPOSER_AUTH) {
+		const composerAuth = process.env.COMPOSER_AUTH
 
-	if (composerAuth) {
-		try {
-			const authData = JSON.parse(composerAuth)
-			const authJsonPath = path.join(process.cwd(), "auth.json")
+		if (composerAuth) {
+			try {
+				const authData = JSON.parse(composerAuth)
+				const authJsonPath = path.join(process.cwd(), filePath ?? "auth.json")
 
-			fs.writeFileSync(authJsonPath, JSON.stringify(authData, null, 4))
+				fs.writeFileSync(authJsonPath, JSON.stringify(authData, null, 4))
 
-			console.log(`auth.json generated successfully at ${authJsonPath}`)
-		} catch (error) {
-			console.error("Failed to generate auth.json:", error.message)
-			process.exit(1)
+				console.log(`auth.json generated successfully at ${authJsonPath}`)
+			} catch (error) {
+				console.error("Failed to generate auth.json:", error.message)
+				process.exit(1)
+			}
 		}
 	}
 }
+
+generateComposerAuthJson()
 
 if (!process.env.SKIP_COMPOSER_INSTALL) {
 	execSync("composer install", { stdio: "inherit" })
--- scripts/after-install.mjs
+++ scripts/after-install.mjs
@@ -1,27 +1,11 @@
 import { execSync } from "child_process"
 import dotenv from "dotenv"
-import fs from "fs"
 import path from "path"
+import { generateComposerAuthJson } from "../apps/bring-app/scripts/after-install.mjs"
 
 dotenv.config({ path: path.resolve(process.cwd(), "apps/bring-app/.env") })
 
-if (!process.env.SKIP_GENERATE_COMPOSER_AUTH) {
-	const composerAuth = process.env.COMPOSER_AUTH
-
-	if (composerAuth) {
-		try {
-			const authData = JSON.parse(composerAuth)
-			const authJsonPath = path.join(process.cwd(), "apps/bring-app/auth.json")
-
-			fs.writeFileSync(authJsonPath, JSON.stringify(authData, null, 4))
-
-			console.log(`auth.json generated successfully at ${authJsonPath}`)
-		} catch (error) {
-			console.error("Failed to generate auth.json:", error.message)
-			process.exit(1)
-		}
-	}
-}
+generateComposerAuthJson("apps/bring-app/auth.json")
 
 if (!process.env.SKIP_COMPOSER_INSTALL) {
 	execSync("yarn install:composer:pkg", { stdio: "inherit" })
