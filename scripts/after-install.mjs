8f5ae1219a85adbe05b7e88fb0402d23fbe16629
Armin Benard <arminbenard@gmail.com>
2024-12-09 14:44:40 +0100

    Update after-install scripts

--- scripts/after-install.mjs
+++ scripts/after-install.mjs
@@ -1,31 +1,31 @@
 import { execSync } from "child_process"
+import dotenv from "dotenv"
 import fs from "fs"
 import path from "path"
+
+dotenv.config({ path: path.resolve(process.cwd(), "apps/bring-app/.env") })
 
 if (!process.env.SKIP_GENERATE_COMPOSER_AUTH) {
 	const composerAuth = process.env.COMPOSER_AUTH
 
 	if (!composerAuth) {
-		// Not necessarily an error
-		console.log("COMPOSER_AUTH is not defined in the .env file.")
-		process.exit(0)
-	}
+		console.warning("COMPOSER_AUTH is not defined in the .env file.")
+	} else {
+		try {
+			const authData = JSON.parse(composerAuth)
+			const authJsonPath = path.join(process.cwd(), "apps/bring-app/auth.json")
 
-	try {
-		const authData = JSON.parse(composerAuth)
-		const authJsonPath = path.join(process.cwd(), "apps/bring-app/auth.json")
+			fs.writeFileSync(authJsonPath, JSON.stringify(authData, null, 4))
 
-		fs.writeFileSync(authJsonPath, JSON.stringify(authData, null, 4))
-
-		console.log(`auth.json generated successfully at ${authJsonPath}`)
-	} catch (error) {
-		console.error("Failed to generate auth.json:", error.message)
-		process.exit(1)
+			console.log(`auth.json generated successfully at ${authJsonPath}`)
+		} catch (error) {
+			console.error("Failed to generate auth.json:", error.message)
+			process.exit(1)
+		}
 	}
 }
 
 if (!process.env.SKIP_COMPOSER_INSTALL) {
-	execSync("composer install", { stdio: "inherit" })
 	execSync("yarn install:composer:pkg", { stdio: "inherit" })
 	execSync("yarn install:composer:app", { stdio: "inherit" })
 }
