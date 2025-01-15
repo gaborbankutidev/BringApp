a008ed1cfaff208b63f66a30ada321fdbbfce531
Gábor Bánkuti <gaborbankuti93@gmail.com>
2025-01-15 10:39:15 +0100

    Remove unused legacy code

--- apps/bring-app/next-app/src/app/api/sentry-example-api/route.ts
+++
@@ -1,7 +0,0 @@
-// // A faulty API route to test Sentry's error monitoring
-// export function GET() {
-// 	throw new Error("Sentry Example API Route Error");
-// 	// return NextResponse.json({ data: "Testing Sentry Error..." });
-// }
-
-export {}
