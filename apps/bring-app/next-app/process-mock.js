cd33e2023ebe713a3f02537c87223d2f66607b60
Gábor Bánkuti <gaborbankuti93@gmail.com>
2025-01-22 10:44:32 +0100

    Update process mock

--- apps/bring-app/next-app/process-mock.js
+++ apps/bring-app/next-app/process-mock.js
@@ -1,9 +1,7 @@
 // Mock process.env for WordPress editor
-const process = {
+export const process = {
 	env: {
 		NODE_ENV: "development",
 		NEXT_PUBLIC_WP_BASE_URL: "http://localhost:8080",
 	},
 }
-
-module.exports = process
