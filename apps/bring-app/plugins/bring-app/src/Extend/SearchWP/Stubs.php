b46dac2c96d1810393467229c20ff6cdfed2c7cb
Gábor Bánkuti <gaborbankuti93@gmail.com>
2025-03-12 08:38:19 +0100

    Fix format

--- apps/bring-app/plugins/bring-app/src/Extend/SearchWP/Stubs.php
+++ apps/bring-app/plugins/bring-app/src/Extend/SearchWP/Stubs.php
@@ -11,14 +11,12 @@ class Query {
 	 *
 	 * @phpstan-ignore-next-line
 	 */
-	public function __construct($term, $args = []) {
-	}
+	public function __construct($term, $args = []) {}
 
 	/**
 	 * @return array<object{"id": string, "source": string, "relevance": string}>
 	 *
 	 * @phpstan-ignore-next-line
 	 */
-	public function get_results() {
-	}
+	public function get_results() {}
 }
